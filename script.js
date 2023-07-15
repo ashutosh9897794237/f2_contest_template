e020796a1422e8180be4edfabb4bc2c2700e703c
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-15 17:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,34 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+   arr
+    .filter((person) => person.profession === "developer")
+    .map((developer) => console.log(developer.name));
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+    arr.forEach((person) => {
+    if (person.profession === "developer") {
+      console.log(person.name);
+    }
+  });
 }
 
-function addData() {
+function addData(newData) {
   //Write your code here, just console.log
+   arr.push(newData);
+  console.log("Data added successfully.");
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+   arr = arr.filter((person) => person.profession !== "admin");
+  console.log("Admin removed successfully.");
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+    arr = arr.concat(newArray);
+  console.log("Arrays concatenated successfully.");
 }
